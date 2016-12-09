c5c9c3eacafda9da1da7c81f7956e05bafe61267
Derek Chapman <derekachapman@gmail.com>
2016-12-09 12:42:32 -1000

    Whitespace at beginning and end of line/parts removed.  Correctly builds dict list of lists.

--- seperate_csv.py
+++ seperate_csv.py
@@ -1,21 +1,28 @@
 file = input('Enter csv name:')
-if file == '' : file = 'csv_sort_test_data.csv'
+if file == '' : file = 'csv_seperate_test_data.csv'
 
 csv_handle = open(file)
 
-# further error checking to make sure it is a number and not out of range of the columns
 seperate_by = int(input('Enter column to seperate by:'))
 if seperate_by == None : seperate_by = 1
 
 header = csv_handle.readline()
-print(header)
 
 sorted_info = dict()
-names_seen = list()
 
-for line in csv_handle.readline():
-	print(line)
+for line in csv_handle:
+	line = line.rstrip()
 	line_parts = line.split(',')
-	print(line_parts)
-	print(line_parts[seperate_by])
+	line_parts = [each.lstrip() for each in line_parts]
+	if line_parts[seperate_by] in sorted_info.keys():
+		sorted_info[line_parts[seperate_by]].append(line)
+	else:
+		sorted_info[line_parts[seperate_by]] = [line]
+	print(sorted_info)
 
+for key in sorted_info.keys():
+	file_name = key + '.csv'
+	print(file_name)
+	# file_handle = open()
+# add in a with statement to auto close the file afterwards
+# further error checking to make sure input is a number and not out of range of the columns
