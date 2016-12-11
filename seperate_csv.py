812ff55d5c36aa43970a129367306bacc4c150b5
Derek Chapman <derekachapman@gmail.com>
2016-12-11 13:18:03 -1000

    Add todo section

--- seperate_csv.py
+++ seperate_csv.py
@@ -1,3 +1,11 @@
+# CVS Seperater
+
+# Todo
+# date stamp each file
+# put them in a date stamped folder
+# add in a with statement to auto close the file afterwards
+# further error checking to make sure input is a number and not out of range of the columns
+
 file = input('Enter csv name:')
 if file == '' : file = 'csv_seperate_test_data.csv'
 
@@ -6,7 +14,7 @@ csv_handle = open(file)
 seperate_by = int(input('Enter column to seperate by:'))
 if seperate_by == None : seperate_by = 1
 
-header = csv_handle.readline()
+header = csv_handle.readline().rstrip()
 
 sorted_info = dict()
 
@@ -18,11 +26,15 @@ for line in csv_handle:
 		sorted_info[line_parts[seperate_by]].append(line)
 	else:
 		sorted_info[line_parts[seperate_by]] = [line]
-	print(sorted_info)
+
+
 
 for key in sorted_info.keys():
 	file_name = key + '.csv'
-	print(file_name)
-	# file_handle = open()
-# add in a with statement to auto close the file afterwards
-# further error checking to make sure input is a number and not out of range of the columns
+	file_handle = open(file_name, 'w')
+	format_line = '{}\n'
+
+	file_handle.write(format_line.format(header))
+	for each in range(len(sorted_info[key])):
+		file_handle.write(format_line.format(sorted_info[key][each]))
+	file_handle.close()
