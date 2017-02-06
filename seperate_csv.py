46f9458ee350d0d66ef20d1c8b3d42c2a233005c
Derek Chapman <derekachapman@gmail.com>
2017-02-06 11:16:42 -1000

    Cleaned up and added comments.

--- seperate_csv.py
+++ seperate_csv.py
@@ -3,24 +3,27 @@
 # Todo
 # add in a with statement to auto close the file afterwards
 # further error checking to make sure input is a number and not out of range of the columns
+# http://stackoverflow.com/questions/13437727/python-write-to-excel-spreadsheet
 
 import datetime as dt
 import os
 
+# get filename and column to seperate the file by from user
 file = input('Enter csv name:')
 if file == '' : file = 'csv_seperate_test_data.csv'
-
 csv_handle = open(file)
 
 seperate_by = int(input('Enter column to seperate by:'))
 if seperate_by == None : seperate_by = 1
 
+# read info but get rid of trailing new line characters
 header = csv_handle.readline().rstrip()
 
+# seperate the info into a dict of lists of strings
 sorted_info = dict()
-
 for line in csv_handle:
 	line = line.rstrip()
+	print(type(line))
 	line_parts = line.split(',')
 	line_parts = [each.lstrip() for each in line_parts]
 	if line_parts[seperate_by] in sorted_info.keys():
@@ -28,11 +31,13 @@ for line in csv_handle:
 	else:
 		sorted_info[line_parts[seperate_by]] = [line]
 
+# setup a date stamp for the folder and individual worksheets / create folder
 today = dt.date.today()
 formatted_date = "{month}-{day}-{year}".format(month=today.month, day=today.day, year=today.year)
 dir_name = '{cwd}/{date}'.format(date=formatted_date, cwd=os.getcwd())
 if not os.path.exists(dir_name) : os.mkdir(dir_name)
 
+# create the individual sheets
 for key in sorted_info.keys():
 	file_name = "{dn}/{key}_{date}.csv".format(key=key, date=formatted_date, dn=dir_name)
 	file_handle = open(file_name, 'w')
