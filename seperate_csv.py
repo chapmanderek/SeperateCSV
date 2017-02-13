e222ac57f0b355ecdb2217d70e14acb88b3cea7c
Derek Chapman <derekachapman@gmail.com>
2017-02-13 13:47:26 -1000

    Records with blank key columns now reported back to user and written to a seperate file named Blanks.

--- seperate_csv.py
+++ seperate_csv.py
@@ -1,10 +1,11 @@
 # CVS Seperater
 
 # Todo
-# loop back the opening section if the input is invalid or out of bounds, add in a q for quit option
+# change user input so it only repeats the section that has a problem
 # allow importing of excel files
 # ask for user input on output settings, # of pages wide etc
 # handle blanks in the selected column gracefully
+# add date stamp to header or footer
 
 import datetime as dt
 import os
@@ -63,25 +64,43 @@ def open_py_output(filename, header, data):
 	return (row_count-1)
 
 # get filename
-file = input('Enter csv name:')
-if file == '' : file = 'test_data.csv'
-csv_handle = open(file)
+print("Thanks for using this file splitter.  Use q to quit.")
+keep_going = False
+while(keep_going == False):
+	keep_going = True
+	file = input('Enter csv name:')
+	if file == '' : file = 'test_data.csv'
+	if file == 'q' or file == 'Q' : exit()
+	
+	try:
+		csv_handle = open(file)
+		# read in the header but get rid of trailing new line characters
+		header = csv_handle.readline().rstrip()
+	except:
+		keep_going = False
+		print('\nSomethings wrong with that filename.  Try again or use q to quit.')
+		continue
 
-# read in the header but get rid of trailing new line characters
-header = csv_handle.readline().rstrip()
 
-# get column to seperate the file by from user error check so exits gracefully
-seperate_by = input('Enter column to seperate by (default is 1):')
-if seperate_by == '' : seperate_by = 1
-elif seperate_by.isdigit() == True : seperate_by = int(seperate_by)
-else:
-	print('Not valid input for some reason')
-	exit()
+	# get column to seperate the file by from user error check so exits gracefully
+	seperate_by = input('Enter column to seperate by (default is 1):')
+	if file == 'q' or file == 'Q' : exit()
+	if seperate_by == '' : seperate_by = 1
+	elif seperate_by.isdigit() == True : seperate_by = int(seperate_by)
 
-# assume user means the 1st column when they say 1 and not the computer 1 which would be the 2nd column 
-# make sure the number is a valid column and exit gracefully if not
-seperate_by -= 1
-if len(header.split(',')) < seperate_by or seperate_by < 0 : print('The number you entered to seperate the worksheet by was no good'); exit()
+	else:
+		print('Not valid input for some reason')
+		keep_going = False
+		continue
+
+	# assume user means 1 index values for the column number
+	# make sure the number is a valid column and exit gracefully if not
+	seperate_by -= 1
+	print('{0} --> user input   {1} --> num columns'.format(seperate_by, len(header.split(','))))
+	if len(header.split(',')) < seperate_by or seperate_by < 0:
+		print('\nThe number you entered to seperate the worksheet by was no good. Try again or use q to quit.')
+		keep_going = False
+		continue
 
 
 # seperate the rest of the info into a dict of lists of strings
@@ -92,6 +111,7 @@ for line in csv_handle:
 	line = line.rstrip()
 	line_parts = line.split(',')
 	line_parts = [each.lstrip() for each in line_parts]
+	
 	if line_parts[seperate_by] in sorted_info.keys():
 		sorted_info[line_parts[seperate_by]].append(line)
 	else:
@@ -107,11 +127,19 @@ if not os.path.exists(dir_name) : os.mkdir(dir_name)
 rows_written = 0
 sheets_created = 0
 for key in sorted_info.keys():
-	file_name = "{dn}/{key}_{date}.xlsx".format(key=key, date=formatted_date, dn=dir_name)
+	keyname = key
+	if keyname == '' : keyname = 'Blanks'
+	file_name = "{dn}/{key}_{date}.xlsx".format(key=keyname, date=formatted_date, dn=dir_name)
 	rows_written += open_py_output(file_name, header, sorted_info[key])
 	sheets_created += 1
 
 print('\n{0} records were read in.'.format(lines_count))
 print('{0} records were written to {1} different sheets. \n'.format(rows_written, sheets_created))
+
+# if there were any blanks in the records print how many were found
+num_blanks = 0
+if '' in sorted_info.keys() : num_blanks = len(sorted_info[''])
+if num_blanks != 0:
+	print('NOTE: {0} records with blank key columns were found and written to a file named Blanks.'.format(num_blanks))
 
 
